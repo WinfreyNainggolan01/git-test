6f8fd8b77f6ba74e35a0bf67e3b39251191d140e
WinfreyNainggolan01 <hiskianggolan@gmail.com>
2026-08-25 20:25:02 +0700

    add fitur product

---
+++ productList.dart
@@ -0,0 +1,15 @@
+
+List<String> productList = [
+  'Product 1',
+  'Product 2',
+  'Product 3',
+  'Product 4',
+  'Product 5',
+];
+
+// fungsi fetch product 
+Future<List<String>> fetchProductList() async {
+  // Simulate a delay to mimic fetching data from an API or database
+  await Future.delayed(Duration(seconds: 2));
+  return productList;
+}
